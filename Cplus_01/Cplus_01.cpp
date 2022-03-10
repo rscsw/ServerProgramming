a419519ba18d8051d52a4ee9fdc97cf5b771838d
rscsw <cathy29@naver.com>
2022-03-10 16:08:27 +0900

    오류 수정

--- Cplus_01/Cplus_01.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include <iostream>
-
-int main()
-{
-    std::cout << "프린트 cout\n";
-}
---
+++ thirdGradeBabo/thirdGradeBabo/Thread01.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+int trigger;
+
+void seokHoon()
+{
+	while (trigger == 0)
+	{
+		cout << "나석훈 : 정석아 사랑해\n";
+		this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+}
+
+void JeongSeok()
+{
+	while (trigger == 0)
+	{
+		this_thread::sleep_for(std::chrono::milliseconds(500));
+		cout << "오정석 : 석훈아 사랑해\n";
+		this_thread::sleep_for(std::chrono::milliseconds(500));
+	}
+}
+
+int main()
+{
+	trigger = 0;
+	cout << "그들의 사랑이 시작된다...\n";
+
+	thread na(seokHoon);
+	thread oh(JeongSeok);
+
+	while (trigger == 0)
+	{
+		cin >> trigger;
+	}
+
+	na.join();
+	oh.join();
+
+	cout << "그들의 사랑이 끝났다...\n";
+}
