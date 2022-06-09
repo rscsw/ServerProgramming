59c94d3ea17e2937dd9232bd715827c759439016
rscsw <cathy29@naver.com>
2022-06-09 14:12:29 +0900

    http 서버 예제

---
+++ thirdGradeBabo/Lecture14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+HttpListener listener = new HttpListener();
+listener.Prefixes.Add("http://127.0.0.1:3000/fuckyou/"); //RestAPI는 꼭 슬래쉬 / 로 끝내줘야한다
+listener.Start();
+Console.WriteLine("Listening...");
+
+HttpListenerContext context = listener.GetContext(); //http listener 객체
+HttpListenerRequest request = context.Request; //응답을 받는 것
+HttpListenerResponse response = context.Response; //응답을 보내는 것
+string responseString = "<html><body>S loves J</body></html>";
+byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+response.OutputStream.Write(buffer, 0, buffer.Length); //버퍼, 오프셋(보낼 버퍼의 시작점), 카운트(보낼 버퍼의 길이)
+response.OutputStream.Close();
+listener.Stop();
